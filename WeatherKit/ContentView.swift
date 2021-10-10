141422bee2137c90cb64d6b64cc3cf440e2c40f9
Nugroho Arief Widodo <nawidodo@gmail.com>
2021-10-10 20:23:52 +0700

    Initial Commit

---
+++ WeatherKit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherKit
+//
+//  Created by Nugroho Arief Widodo on 10/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherKit/WeatherKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherKitApp.swift
+//  WeatherKit
+//
+//  Created by Nugroho Arief Widodo on 10/10/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
