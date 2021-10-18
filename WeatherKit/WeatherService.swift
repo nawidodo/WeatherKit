1ba02890065c7d4cb21cb43c4392be64b5cdf421
Nugroho Arief Widodo <nawidodo@gmail.com>
2021-10-18 17:28:14 +0700

    Cleanup

--- WeatherKit/WeatherService.swift
+++ WeatherKit/WeatherService.swift
@@ -12,6 +12,7 @@ class WeatherService {
     
     static let shared = WeatherService()
     private let url = "http://api.openweathermap.org/data/2.5/weather?lat={{lat}}&lon={{lon}}&appid=dbb2dd22ce8ddfc4498635ace6ea59dd"
+    
     func getWeather(lat: Double, lon: Double) -> AnyPublisher<String?, Error> {
         var target = url.replacingOccurrences(of: "{{lat}}", with: "\(lat)")
         target = target.replacingOccurrences(of: "{{lon}}", with: "\(lon)")
